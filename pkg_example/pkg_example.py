db741cad6b1cadadf6bbe4328a3f2ad4fa51ac82
Northbreeze <nasim.taba2@gmail.com>
2021-08-06 14:05:07 -0700

    added pandas as a dependency

--- pkg_example/pkg_example.py
+++ pkg_example/pkg_example.py
@@ -1,1 +1,8 @@
 """Main module."""
+import pandas as pd
+
+def catbind(a, b):
+    concatenated = pd.concat([pd.Series(a.astype("str")),
+    pd.Series(b.astype("str"))])
+
+    return pd.Categorial(concatenated)
